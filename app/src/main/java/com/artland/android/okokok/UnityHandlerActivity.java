ecbd0d383c9b91da7960b21332adc105460b8ea5
Minf <rjclsms@gmail.com>
2021-09-23 19:42:37 +0900

    Initial commit

---
+++ app/src/main/java/com/artland/android/okokok/UnityHandlerActivity.java
@@ -0,0 +1,29 @@
+package com.artland.android.okokok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class UnityHandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unity_handler);
+
+        Button buttonClick = findViewById(R.id.buttonClick);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UnityHandlerActivity.this, UnityPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
